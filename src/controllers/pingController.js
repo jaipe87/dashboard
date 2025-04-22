101b337e767dd170d21db5c947305df603f13fc3
Jaime Pacheco <millepacheco87@gmail.com>
2025-04-21 21:31:15 -0500

    actualice el ping

--- src/controllers/pingController.js
+++ src/controllers/pingController.js
@@ -1,8 +1,8 @@
 const connection = require('../models/db')
 
 module.exports.ping = (req, res) => {
-    const consult = `CALL sp_utilidades_anuales(1, 2023, 'OFICINA')`;
-
+    const consult = `CALL sp_ping('OFICINA')`;
+  
     try {
         connection.query(consult, (err, results) => {
             console.log(results)
